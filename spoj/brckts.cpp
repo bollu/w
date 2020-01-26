94b759f50777036780997c7f9ab7441847ad934a
Siddharth Bhat <siddu.druid@gmail.com>
2020-01-27 04:01:05 +0530

    solved brackets

--- spoj/brckts.cpp
+++ spoj/brckts.cpp
@@ -8,65 +8,48 @@ B flip(B b) { return B {.l = b.r, .r  = b.l }; }
 B join(B a, B b) {  I c = min(a.r, b.l); return B (a.l + b.l - c, a.r + b.r - c); } 
 // keep pair of int: num unmatched )), num unmatched ((
 B a[N];
+B aseg[N];
 int n;
 
-/*
+void build(int nl, int nr, int segix) {
+  if (nl == nr) { aseg[segix] = a[nl]; return;}
+  int mid = (nl + nr) / 2;
+  build(nl, mid, segix*2); build(mid+1, nr, segix*2+1);
+  aseg[segix] = join(aseg[segix*2], aseg[segix*2+1]);
+}
+
+B query(int ql, int qr, int nl, int nr, int segix) {
+  if (qr < nl || nr < ql) { return B(); }
+  if (ql <= nl && nr <= qr) { return aseg[segix]; }
+  int mid = (nl + nr) / 2;
+  join(query(ql, qr, nl, mid, segix*2), query(ql, qr, mid+1, nr, segix*2+1));
+
+}
+
+void flipseg(int q, int nl, int nr, int segix) {
+  if (q < nl || nr < q) { return; }
+  if (nl == nr) { if (q == nl) { aseg[segix] = flip(aseg[segix]); } return; }
+  int mid = (nl + nr) / 2;
+  if (q <= mid) { flipseg(q, nl, mid, segix*2); }
+  else { flipseg(q, mid+1, nr, segix*2+1); }
+  aseg[segix] = join(aseg[segix*2], aseg[segix*2+1]);
+}
+
 void test() {
     cin >> n;
-    // 1 -> n | (i - 0 + n)
-    for(int i = n; i < 2*n; ++i) { 
+    for(int i = 1; i <= n; ++i) {
         char c; cin >> c; a[i] = B(int(c == ')'), int(c =='('));
     }
-
-    // build
-    for(int i = n-1; i >= 1; i--) {
-        a[i] = join(a[i*2], a[(i*2)^1]);
-    }
-
-    // for(int i = n; i < 2*n; ++i) cout << (a[i].l == 1 ? ')' : '('); cout << "\n";
+    build(1, n, 1);
 
     int m; cin>>m; // cout << "M: " << m << "\n";
     while(m--) {
         int k; cin >> k;
         if (k == 0) {
-            if (a[1].l == 0 && a[1].r == 0) { cout << "YES\n"; } 
+            if (aseg[1].l == 0 && aseg[1].r == 0) { cout << "YES\n"; } 
             else { cout << "NO\n"; }
         } else {
-            k -= 1; k += n; a[k] = flip(a[k]); 
-            while(k >= 1) { k /= 2; a[k] = join(a[k*2], a[(k*2)^1]); }
-        }
-    }
-}
-*/
-
-int pow2(int n) {
-  int p = 1; while(n--) p *= 2; return p;
-}
-
-void test() {
-    cin >> n;
-    int segstart = pow2(ceil(log2(n)));
-    // 1 -> n | (i - 0 + n)
-    for(int i = segstart; i < segstart + n; ++i) { 
-        char c; cin >> c; a[i] = B(int(c == ')'), int(c =='('));
-    }
-
-    // build
-    for(int i = segstart-1; i >= 1; i--) {
-        a[i] = join(a[i*2], a[(i*2)^1]);
-    }
-
-    // for(int i = n; i < 2*n; ++i) cout << (a[i].l == 1 ? ')' : '('); cout << "\n";
-
-    int m; cin>>m; // cout << "M: " << m << "\n";
-    while(m--) {
-        int k; cin >> k;
-        if (k == 0) {
-            if (a[1].l == 0 && a[1].r == 0) { cout << "YES\n"; } 
-            else { cout << "NO\n"; }
-        } else {
-            k -= 1; k += segstart; a[k] = flip(a[k]); 
-            while(k >= 1) { k /= 2; a[k] = join(a[k*2], a[(k*2)^1]); }
+          flipseg(k, 1, n, 1);
         }
     }
 }
