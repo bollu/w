482144a4ad9dcbab792f64568119ea33e6c2fa6a
Siddharth Bhat <siddu.druid@gmail.com>
2020-05-29 03:11:07 +0530

    make split pure, easier to reason about

--- elementary/treap.cpp
+++ elementary/treap.cpp
@@ -13,7 +13,6 @@ template<typename K, typename V, int INFTY=100000000> struct treap {
     };
     node *root = nullptr;
 
-    int count() const;
     node *lookup(const K k) const { return lookup(k, root); } 
     node *lookup(const K k, node *root) const {
         if (!root) { return nullptr; }
@@ -22,7 +21,7 @@ template<typename K, typename V, int INFTY=100000000> struct treap {
     }
 
     // *destructively* merge nodes p and q
-    node *merge(node *&p, node *&q) {
+    node *merge(node *&p, node *&q) const {
         if (!p || !q) { return p ? p : q; }
         // p is always parent (closer to root on min-heap)
         if (p->pr > q->pr) { swap(p, q); }
@@ -40,43 +39,41 @@ template<typename K, typename V, int INFTY=100000000> struct treap {
         else { erase(k, k < root->k  ? root->l : root->r); }
     }
 
-    void insert(K k, V v) { 
-        insert(new node(k, v, rand() % 10), root);
-    }
 
-    void insert(node *n, node *&root) {
-        if (!root) { root = n; return; }
-        if (root->k == n->k) { root->v = n->v; return;  }
 
-        if (n->pr < root->pr) {
-            //   INCORRECT:
-            //   n    
-            //   k:8 ----\
-            //        root
-            //      /--k:10--\
-            //     k:4      k:16
-            //       \
-            //       k:9
-            // ================
-            //   CORRECT:
-            //      n    
-            //  /---8---------\
-            // k:4           root
-            //          /----10----\
-            //         k:9         k:16
-
+    // how does this work?
+    pair<node *, node*> split(K k, node *cur, pair<node *, node*>lr) const {
+        if (!cur) { return lr; }
+        else if (k < cur->k) {
+            node *nextl, *nextr;
+            tie(nextl, nextr) = split(k, cur->l, make_pair(cur->l, lr.second));
+            return make_pair(nextl, cur);
+        } else { 
+            node *nextl, *nextr;
+            tie(nextl, nextr) = split(k, cur->r, make_pair(lr.first, cur->r));
+            return make_pair(cur, nextr);
         }
-        else { insert(n, n->k < root->k ? root->l : root->r); }
     }
 
-    void check(node *n) {
+    void insert(K k, V v) { 
+        if (node *n = lookup(k)) { n->v = v; return; }
+        insert(new node(k, v, rand() % 1000), root);
+    }
+
+    void insert(node *n, node *&cur) const {
+        if (!cur) { cur = n; return; }
+        else if (cur->pr < n->pr) { insert(n, n->k < cur->k ? cur->l : cur->r); }
+        else { tie(n->l, n->r) = split(n->k, cur, make_pair(nullptr, nullptr)); cur = n; }
+    }
+
+    void check(node *n) const {
         if (n == nullptr) return;
-        if (n->l) { assert(n->pr <= n->l->pr); assert(n->l->k < n->k); check(n->l); } 
+        else if (n->l) { assert(n->pr <= n->l->pr); assert(n->l->k < n->k); check(n->l); } 
         if (n->r) { assert(n->pr <= n->r->pr); assert(n->r->k >= n->k); check(n->r); } 
     }
 
-    void print() { print(root, 0); }
-    void print(node *n, int depth) {
+    void print() const { print(root, 0); }
+    void print(node *n, int depth) const {
         if (!n) return;
         // for(int i = 0; i < depth; ++i) cout << ' ';
         cout << "[pr(" << n->pr << ") k(" << n->k << ") v(" << n->v <<")]\n";
@@ -89,6 +86,9 @@ template<typename K, typename V, int INFTY=100000000> struct treap {
             print(n->r, depth+4);
         }
     }
+
+    int size() const { return size(root); }
+    int size(node *n) const { if (!n) return 0; return 1 + size(n->l) + size(n->r); }
 };
 
 int main() { 
@@ -98,13 +98,15 @@ int main() {
     map<int, int> m;
     const int KEYSPACE = 10;
 
-    for(int i = 0; i < 10; ++i) {
+    for(int i = 0; i < 10000; ++i) {
         int k = rand() % KEYSPACE;
         int v = rand() % 100;
         cout << "inserting map[" << k << "] = " << v << "\n";
         m[k] = v;
         tr.insert(k, v);
+        tr.print();
         tr.check(tr.root);
+        assert(m.size() == tr.size());
     }
     tr.print();
 
@@ -115,16 +117,18 @@ int main() {
         int v2 = it == nullptr ? -42 : it->v;
         cout << "lookup up (" << i << "): map[i] = " << v1
              << " | tr[i] = " << v2 << "\n";
+        tr.print();
         assert(v1 == v2);
     }
 
 
-    for(int i = 0; i < 100; ++i) {
+    for(int i = 0; i < 1000; ++i) {
         int k = rand() % KEYSPACE;
         auto map_it = m.find(k);
-        auto tr_it = tr.lookup(i);
+        auto tr_it = tr.lookup(k);
 
         assert((map_it == m.end()) == (tr_it == nullptr));
+        if (map_it == m.end()) { continue; }
         m.erase(map_it);
         tr.erase(k);
     }
