ba9018eb9d223c6b4e5e4ac4ee16f971112396ca
Siddharth Bhat <siddu.druid@gmail.com>
2020-10-19 15:06:22 +0530

    add some thoughts

--- cses/maximum-subarray-sum.cpp
+++ cses/maximum-subarray-sum.cpp
@@ -47,17 +47,22 @@ namespace f1 {
 int main() {
     ll n;
     cin >> n;
-    vector<ll> xs(n);
+
+    // think of this as height δs, and we are trying to calculate the max
+    // height of a mountain
+    vector<ll> xs(n); 
     for (ll i = 0; i < n; ++i) {
         cin >> xs[i];
     }
 
+    // NOTE: this only works we are interested in the *signed* height of mountain
+    // ie, we only want lower-to-higher, not higher-to-lower.
     ll best = -INFTY;
-    ll lowest = 0;
-    ll height = 0;
+    ll lowest = 0; // floor/beginning of mountain: min height seen so far
+    ll height = 0; // current height: integral of δs
     for(int i = 0; i < xs.size(); ++i) {
         height += xs[i];
-        best = max(best, height - lowest);
+        best = max(best, height - lowest); // elevation is 
         lowest = min(lowest, height);
     }
     cout << best << "\n";
