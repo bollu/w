5b6f19dd78f94c10c9355c743797ca7dc3421b14
Siddharth Bhat <siddu.druid@gmail.com>
2020-10-27 20:12:17 +0530

    add book shop

--- cses/book-shop.cpp
+++ cses/book-shop.cpp
@@ -32,15 +32,159 @@ ostream &operator<<(ostream &o, const pair<T1, T2> &p) {
     return o << "(" << p.first << ", " << p.second << ")";
 }
 
+
+// This only works if we can pick the same book multiple times!
+// we want zero or one!
+// Best solution for problem:
+// 4 10
+// 4 8 5 3
+// 5 12 8 1
+//  Unrestriced: 
+//    pick book with price=5, #pages=8 _twice_, giving us 16 pages.
+//
+//
+// 0/1:
+//   pick book with price=5, price=4 to give 8 + 5 = 13 pages.
+namespace f0 {
+static const int INFTY = 1e9 + 42;
+static const int MAXPRICE = 1e5 + 5;
+int dp[MAXPRICE];
+
+struct Book {
+    int pages;
+    int price;
+    bool operator<(const Book &o) { return price < o.price; };
+};
+
 int main() {
     std::ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n, x; 
-    vector<int> hs(n);
-    vector<int> ss(n);
-    for(int i = 0; i < n; ++i) { cin >> hs[i]; }
-    for(int i = 0; i < n; ++i) { cin >> ss[i]; }
+    int n, maxprice;
+    cin >> n >> maxprice;
 
+    vector<Book> books(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> books[i].price;
+    }
+    for (int i = 0; i < n; ++i) {
+        cin >> books[i].pages;
+    }
+
+
+    sort(books.begin(), books.end());
+
+    for(int p = 1; p <= maxprice; ++p) {
+        cerr << "  p:"  <<p << "\n";
+        for(int i = 0; i < n; ++i) {
+            const Book b = books[i];
+            if (b.price > p) { continue; }
+            int cur = b.pages + dp[p - b.price];
+            if (cur > dp[p]) {
+                cerr << "    best=" << cur << " i="  << i << 
+                    " pages=" << b.pages << " price=" << b.price <<" rest=dp[" << p - b.price << "]=" << 
+                    dp[p-b.price] << "\n";
+                dp[p] = cur;
+            }
+            // dp[p] = max<int>(dp[p], b.pages + dp[p - b.price]);
+        }
+    }
+
+
+    cout << dp[maxprice] << "\n";
 
     return 0;
+}
+}
+
+//Correctly perform 0/1 dp, but this TLEs.
+namespace f1 {
+static const int INFTY = 1e9 + 42;
+static const int MAXPRICE = 1e5 + 5;
+static const int MAXBOOKS = 1e3 + 5;
+int dp[MAXPRICE][MAXBOOKS];
+
+struct Book {
+    int pages;
+    int price;
+    bool operator<(const Book &o) { return price < o.price; };
+};
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, maxprice;
+    cin >> n >> maxprice;
+
+    vector<Book> books(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> books[i].price;
+    }
+    for (int i = 0; i < n; ++i) {
+        cin >> books[i].pages;
+    }
+
+
+    sort(books.begin(), books.end());
+
+
+    for(int i = 0; i < books.size(); ++i) {
+        const Book b = books[i];
+        for(int p = b.price; p <= maxprice; ++p) {
+            dp[p][i+1] = max<int>(dp[p][i], b.pages + dp[p - b.price][i]);
+        }
+    }
+    cout << dp[maxprice][books.size()] << "\n";
+
+    return 0;
+}
+}
+
+// Use same trick as coins. Switch order of processing. Process books, not
+// prices. Use double buffering instead of N buffers.
+namespace f2 {
+static const int INFTY = 1e9 + 42;
+static const int MAXPRICE = 1e5 + 5;
+static const int MAXBOOKS = 1e3 + 5;
+ll dp[MAXPRICE][2];
+
+struct Book {
+    ll pages;
+    ll price;
+    bool operator<(const Book &o) { return price < o.price; };
+};
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, maxprice;
+    cin >> n >> maxprice;
+
+    vector<Book> books(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> books[i].price;
+    }
+    for (int i = 0; i < n; ++i) {
+        cin >> books[i].pages;
+    }
+
+
+    sort(books.begin(), books.end());
+
+    bool ixcur = 1;
+    for(int i = 0; i < n; ++i) {
+        bool ixnext = ixcur^1;
+        const Book b = books[i];
+        for(int p = b.price; p <= maxprice; ++p) {
+            dp[p][ixnext] = max<ll>(dp[p][ixcur], b.pages + dp[p - b.price][ixcur]);
+        }
+        ixcur = ixnext;
+    }
+    cout << dp[maxprice][ixcur] << "\n";
+
+    return 0;
+}
+}
+
+int main() {
+    f2::main();
 }
--- cses/edit-distance.cpp
+++ cses/edit-distance.cpp
@@ -34,30 +34,37 @@ ostream &operator<<(ostream &o, const pair<T1, T2> &p) {
 
 static const int INFTY = (1e9 + 42);
 static const int NMAX = 5000 + 5;
+namespace f0 {
 bool dpd[NMAX][NMAX];
 int dp[NMAX][NMAX];
 
 int edit(string &s, string &t, int i, int j) {
-    if (dpd[i][j]) { return dp[i][j]; }
+    if (dpd[i][j]) {
+        return dp[i][j];
+    }
 
     int ans = INFTY;
     // delete everything in t to match s=""
-    if (i == s.size()) { return t.size() - j; }
+    if (i == s.size()) {
+        return t.size() - j;
+    }
     // insert everything in s to t="" to match s
-    if (j == t.size()) { return s.size() - i; }
+    if (j == t.size()) {
+        return s.size() - i;
+    }
 
     // 1 to insert the letter at position `i`
-    ans = min<int>(ans, 1 + edit(s, t, i+1, j));
+    ans = min<int>(ans, 1 + edit(s, t, i + 1, j));
     // 1 to delete the letter at position `j`
-    ans = min<int>(ans, 1 + edit(s, t, i, j+1));
+    ans = min<int>(ans, 1 + edit(s, t, i, j + 1));
 
     // edit 'i' to 'j'
-    ans = min<int>(ans, (s[i] == t[j] ? 0 : 1)  +  edit(s, t, i+1, j+1));
+    ans = min<int>(ans, (s[i] == t[j] ? 0 : 1) + edit(s, t, i + 1, j + 1));
     dpd[i][j] = true;
     dp[i][j] = ans;
 
-
-    // cout << "edit '" << s.substr(i) << "' '" << t.substr(j) << "' = " << ans << "\n";
+    // cout << "edit '" << s.substr(i) << "' '" << t.substr(j) << "' = " << ans
+    // << "\n";
     return ans;
 }
 
@@ -74,3 +81,92 @@ int main() {
     return 0;
 }
 
+}  // namespace f0
+
+// bottom up DP
+namespace f1 {
+int dp[NMAX][NMAX];
+
+int main() {
+    std::string s, t;
+
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> s >> t;
+
+    int n = s.size();
+    int m = t.size();
+
+    // empty strings match each other.
+    dp[n][m] = 0;
+
+    // need to insert all characters into t
+    for (int i = 0; i <= n; ++i) {
+        dp[i][m] = n - i;
+    }
+
+    // need to delete all chars from t to make it equal to s=""
+    for (int j = 0; j <= m; ++j) {
+        dp[n][j] = m - j;
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = m - 1; j >= 0; --j) {
+            dp[i][j] = INFTY;
+            dp[i][j] = std::min<int>(dp[i][j],
+                                     (s[i] == t[j] ? 0 : 1) + dp[i + 1][j + 1]);
+            dp[i][j] = std::min<int>(dp[i][j], 1 + dp[i + 1][j]);
+            dp[i][j] = std::min<int>(dp[i][j], 1 + dp[i][j + 1]);
+        }
+    }
+
+    cout << dp[0][0] << "\n";
+    return 0;
+}
+
+}  // namespace f1
+
+// order reversed bottom up DP: j then i. TLE because of caching.
+namespace f2 {
+int dp[NMAX][NMAX];
+
+int main() {
+    std::string s, t;
+
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> s >> t;
+
+    int n = s.size();
+    int m = t.size();
+
+    // empty strings match each other.
+    dp[n][m] = 0;
+
+    // need to insert all characters into t
+    for (int i = 0; i <= n; ++i) {
+        dp[i][m] = n - i;
+    }
+
+    // need to delete all chars from t to make it equal to s=""
+    for (int j = 0; j <= m; ++j) {
+        dp[n][j] = m - j;
+    }
+
+    for (int j = m - 1; j >= 0; --j) {
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][j] = INFTY;
+            dp[i][j] = std::min<int>(dp[i][j],
+                                     (s[i] == t[j] ? 0 : 1) + dp[i + 1][j + 1]);
+            dp[i][j] = std::min<int>(dp[i][j], 1 + dp[i + 1][j]);
+            dp[i][j] = std::min<int>(dp[i][j], 1 + dp[i][j + 1]);
+        }
+    }
+
+    cout << dp[0][0] << "\n";
+    return 0;
+}
+
+}  // namespace f2
+
+int main() { f2::main(); }
