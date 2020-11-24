13fd5f49a6a97bb4b2615ebd3cecb21081833743
Siddharth Bhat <siddu.druid@gmail.com>
2020-11-24 10:54:04 +0530

    add incorrect algo for planets and kingdoms. think about why this is wrong

--- cses/planets-and-kingdoms.cpp
+++ cses/planets-and-kingdoms.cpp
@@ -37,27 +37,31 @@ template <typename T1, typename T2>
 ostream &operator<<(ostream &o, const pair<T1, T2> &p) {
     return o << "(" << p.first << ", " << p.second << ")";
 }
-// calculate SCC
+
+// Implement *incorrect* algorithm to understand where it breaks
+namespace f0 {
 const int N = 1e5 + 10;
 bool vis[N];
 vector<int> es[N];
-int comp[N];
+vector<int> exitvs; // vertices pushed back as we exit.
 
-vector<int> esrev[N];
-bool visrev[N];
-int comprev[N];
-
-void dfs(int v, int c)  {
-    comp[v] = c;
+void dfs(int v)  {
     vis[v] = true;
     for(int w : es[v]) {
-        if (vis[w]) { continue; } dfs(w, c);
+        if (vis[w]) { continue; } dfs(w);
     }
+    exitvs.push_back(v);
 }
+vector<int> esrev[N];
+bool visrev[N];
+
+int comp[N];
 void dfsrev(int v, int c)  {
-    comprev[v] = c;
+    if (visrev[v]) return; 
+    comp[v] = c;
     visrev[v] = true;
-    for(int w : esrev[v]) {
+    for(int w: es[v]) {
+    // for(int w : esrev[v]) {
         if (visrev[w]) { continue; } dfsrev(w, c);
     }
 }
@@ -67,6 +71,7 @@ int n, m;
 // https://www.hackerearth.com/practice/algorithms/graphs/strongly-connected-components/tutorial/
 // https://www.youtube.com/watch?v=9Wbej7Fy5Lw
 // https://www.youtube.com/watch?v=iYJqgMKYsdI
+// https://www.iarcs.org.in/inoi/online-study-material/topics/scc.php
 int main() {
     std::ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -77,8 +82,43 @@ int main() {
         esrev[b].push_back(a);
     }
 
-    for(int i = 1; i <= n; ++i) { if (!vis[i]) { dfs(i, i); } }
-    for(int i = 1; i <= n; ++i) { if (!visrev[i]) { dfsrev(i, i); } }
+    for(int i = 1; i <= n; ++i) { if (!vis[i]) { dfs(i); } }
+
+   // a<->b->c<->d
+   // DFS from a: a -> b -> c -> d
+   // reverse: d will be a final SCC component.
+   // will find d<->c
+   // remove d, c.
+   // next will find b. will do b<->a 
+    for(int i = 0; i < exitvs.size(); i++) {
+        int v = exitvs[i];
+        if (!visrev[v]) { dfsrev(v, v); }
+    }
+
+    map<int, int> comp2ix;
+    for(int i = 1; i <= n; ++i) {
+        auto it = comp2ix.find(comp[i]);
+        if (it == comp2ix.end()) {
+            comp2ix[comp[i]] =  comp2ix.size()+1;
+        }
+    }
+
+    cout << comp2ix.size() << "\n";
+    for(int i = 1; i <= n; ++i) {
+        cout << comp2ix[comp[i]] << " \n"[i==n];
+    }
 
     return 0;
 }
+}
+
+int main() { return f0::main(); }
+
+// a<->b -> c<->d
+//     |      |
+//     v      v     
+//     e<---->f
+
+// dfs visit: (a (b (e (f f) e) (c (d d) c) b) a)
+// exit times: f e d c b a
+// f e; d c; b a
