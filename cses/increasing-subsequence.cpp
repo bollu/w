7467fc0d65309cbc84abfa7236b6038ca3d8b904
Siddharth Bhat <siddu.druid@gmail.com>
2021-06-29 00:17:34 +0530

    solve LIS with segtree

--- cses/increasing-subsequence.cpp
+++ cses/increasing-subsequence.cpp
@@ -33,22 +33,82 @@ ostream &operator<<(ostream &o, const pair<T1, T2> &p) {
 }
 
 static const int NMAX = 2 * int(1e5) + 10;
+int tree[4*NMAX+10];
+int dp[NMAX+10];
 
-//int dp[NMAX][NMAX];
-int lenbest[NMAX];
+// half-open: nr == nl + 1 \/ nr >= nl + 2
+// [nl, nr): nr >= nl + 2
+// (nl + nr)/2 >= nl + 1
+// nl < nl + 1 <= mid < nr
+
+// closed-closed:
+// sum of adjacent numbers is always odd...
+// [nl, nr]: nl == nr or nr >= nl + 1
+// [nl, mid-1] [mid, nr]
+
+
+// find max value in range [ql..qr]. Node is at [nl...nr].
+int rangeq(int ql, int qr, int nix, int nl, int nr) {
+    // int out = -1;
+    // for(int i = ql; i <= qr; i++) {
+    //     out = max(out, tree[i]);
+    // }
+    // cout << "rangeq[" << ql << ".." << qr << "]=" << out << "\n";
+    // return out;
+
+
+    if (qr < nl || nr < ql) { return -100; }
+    if (ql <= nl && nr <= qr) { return tree[nix]; }
+    int nmid = (nl + nr)/2;
+    return max(rangeq(ql, qr, 2*nix, nl, nmid), rangeq(ql, qr, nix*2+1, nmid+1, nr));
+}
+
+void setq(int ix, int val, int nix, int nl, int nr) {
+    // cout << "setq(" << ix << "=" << val << ")\n";
+    // tree[ix] = val;
+    // return;
+
+    // cerr << ix << "=" << val << " | " << nix << "[" << nl << ", " << nr << "]\n";
+    if (ix < nl || nr < ix) { return; }
+    if (nl == nr) { tree[nix] = val; return; }
+    int nmid = (nl + nr)/2;
+    if (ix <= nmid) { setq(ix, val, nix*2, nl, nmid); }
+    else { setq(ix, val, nix*2+1, nmid+1, nr); }
+    tree[nix] = max(tree[nix*2], tree[nix*2+1]); 
+}
 
 int main() {
     std::ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int n; cin >> n;
     vector<int> xs(n);
-    for(int i = 1; i < n; ++i) { cin >> xs[i]; }
+    std::set<int> uniqs;
+    for(int i = 0; i < n; ++i) { 
+        cin >> xs[i];
+        uniqs.insert(xs[i]); 
+    }
 
-    // [small univ] --> [our indexes, weighted by our array]
-    // compress of ordering on array to ordering on [0..n-1].
-    // We compute the pushforward by pulling back data, because the total
-    // ordering is somehow like a dual space?
+    // create compressed version that has the right size.
+    map<int, int> compress;
+    for(int i : uniqs) { compress[i] = compress.size(); }
 
-    vector<int> decompress(n); // decompress: univ -> arr(ix)
+    int best = 0;
+    for(int i = 0; i < n; ++i) {
+        if (i == 0) { 
+            setq(compress[xs[i]], 1, 1, 0, NMAX);
+            best = 1;
+        }
+        else {
+            // >>> math.log(1e5)
+            // 11.512925464970229
+            int nlt = rangeq(0, compress[xs[i]]-1, 1, 0, NMAX); 
+            setq(compress[xs[i]], 1 + nlt, 1, 0, NMAX);
+            best = max(best, 1 + nlt);
+        }
+    }
+
+    cout << best << "\n";
+
+
     return 0;
 }
