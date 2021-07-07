3c40e96846d52bd79f00c6f6d0df1fdd48f65caf
Siddharth Bhat <siddu.druid@gmail.com>
2021-07-07 20:04:00 +0530

    add current files

---
+++ forces/1543/a.cpp
@@ -0,0 +1,18 @@
+#include <assert.h>
+#include<iostream>
+#include<vector>
+#include<map>
+#include<set>
+#include<utility>
+#include<algorithm>
+#include <queue>
+#include <stack>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+}
+
+
+
